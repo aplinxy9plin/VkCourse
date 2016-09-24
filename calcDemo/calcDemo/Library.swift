175e6022b51f9777f363e3322080a64a1aa81ecc
Michael Galperin <m.galp509@gmail.com>
2016-09-24 14:27:06 +0300

    cleaning

--- calcDemo/calcDemo/Library.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Library.swift
-//  CalcDemo
-//
-//  Created by Michael Galperin on 24.09.16.
-//
-//
-
-import Foundation
--- calcDemo/calcDemo/TextfieldInset.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TextfieldInset.swift
-//  CalcDemo
-//
-//  Created by Michael Galperin on 24.09.16.
-//
-//
-
-import Foundation
